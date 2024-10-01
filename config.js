69d795c449ac58df675767a4941daa4048cfd02d
kentaurse <kentaurse0212@gmail.com>
2024-10-01 16:05:02 +0900

    fix: allow cors error

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   secret: "super secret passphrase",
   db_collection_prefix: "ciis_",
   lang: "en",
-  allowed_origin: ["*"],
+  allowed_origin: ["https://minion-eshop-frontend.vercel.app", "*"],
 
   expiresIn: 3600 * 72, // 3 days
   db_url: "mongodb+srv://vercel-admin-user:telxWP2jPuiphGpN@cluster0.nekraz9.mongodb.net/minions",
--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ module.exports = {
   MONGOURI: "mongodb+srv://vercel-admin-user:telxWP2jPuiphGpN@cluster0.nekraz9.mongodb.net/minions",
   secretOrKey: "secret",
   allowed_origin: [
-    "*",
+    "https://minion-eshop-frontend.vercel.app",
+    "*"
   ],
 };
